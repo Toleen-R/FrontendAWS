04353f73f04648e10b9560dbe6167f842a711e57
David Heidari <davidheidari2@gmail.com>
2024-11-04 15:23:38 +0100

    change vit.confiq.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [react()],
+  "process.env": {
+    REACT_APP_ENVIRONMENT: JSON.stringify(process.env.REACT_APP_ENVIRONMENT),
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
